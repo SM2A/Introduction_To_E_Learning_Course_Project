b626f5adc1bc116173a6b16d9f210435a275d1f4
Seyed Mohammad Amin Atyabi <seyedamin1990@gmail.com>
2023-05-27 23:07:20 +0330

    Sample scrollable list

--- app/src/main/java/com/example/tsl/MainActivity.kt
+++ app/src/main/java/com/example/tsl/MainActivity.kt
@@ -1,17 +1,21 @@
 package com.example.tsl
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -35,6 +39,10 @@ class MainActivity : ComponentActivity() {
                     "تابلوهای راهنما مسیر",
                     "تابلوهای محلی",
                     "علائم جاده‌ای",
+                    "علائم جاده‌ای1",
+                    "2علائم جاده‌ای",
+                    "3علائم جاده‌ای",
+                    "4علائم جاده‌ای",
                     "آزمون نهایی",
                     "آزمون سنج یادگیری"
                 ),
@@ -52,33 +60,60 @@ fun LessonList(
     list: List<String>,
     modifier: Modifier = Modifier
 ) {
-    Column(
+    LazyColumn(
         modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        list.forEach {
-            Box(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(4.dp)
-                    .background(
-                        color = Color(0xFFCEFF95),
-                        shape = RoundedCornerShape(8.dp)
-                    )
-                    .border(
-                        width = 1.dp,
-                        color = Color(0xFF4CAF50),
-                        shape = RoundedCornerShape(8.dp)
-                    )
-                    .padding(top = 8.dp, bottom = 8.dp),
-                contentAlignment = Alignment.Center
-            ) {
-                Text(
-                    text = it,
-                    fontSize = 32.sp,
-                    color = Color(0xFF858585)
-                )
+        items(
+            items = list,
+            key = { msg -> msg }
+        ) {
+            LessonItem(text = it)
+        }
+    }
+}
+
+@Composable
+fun LessonItem(
+    text: String
+) {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(4.dp)
+            .background(
+                color = Color(0xFFCEFF95),
+                shape = RoundedCornerShape(8.dp)
+            )
+            .border(
+                width = 1.dp,
+                color = Color(0xFF4CAF50),
+                shape = RoundedCornerShape(8.dp)
+            )
+            .padding(top = 8.dp, bottom = 8.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        Button(
+            modifier = Modifier
+                .fillMaxHeight()
+                .fillMaxWidth(),
+            colors = ButtonDefaults.buttonColors(
+                backgroundColor = Color.Transparent,
+                contentColor = Color.Transparent
+            ),
+            elevation = ButtonDefaults.elevation(
+                defaultElevation = 0.dp,
+                pressedElevation = 0.dp
+            ),
+            onClick = {
+                Log.e("TAG", "LessonItem: $text")
             }
+        ) {
+            Text(
+                text = text,
+                fontSize = 32.sp,
+                color = Color(0xFF525252)
+            )
         }
     }
 }
