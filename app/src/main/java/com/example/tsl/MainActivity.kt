5ca6aed6d27bd62ab181ca8d21cbf63fa3dd4b39
atyabi <atyabi@mahsan.co>
2023-05-27 13:57:26 +0330

    Simple lessons list

--- app/src/main/java/com/example/tsl/MainActivity.kt
+++ app/src/main/java/com/example/tsl/MainActivity.kt
@@ -3,12 +3,82 @@ package com.example.tsl
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            LessonList(
+                list = listOf(
+                    "آزمون ورودی",
+                    "تابلوهای دستوری",
+                    "تابلوهای هشداری",
+                    "تابلوهای اطلاعاتی",
+                    "تابلوهای بازدازنده و انتظامی",
+                    "تابلوهای آگاهی دهنده",
+                    "تابلوهای راهنما مسیر",
+                    "تابلوهای محلی",
+                    "علائم جاده‌ای",
+                    "آزمون نهایی",
+                    "آزمون سنج یادگیری"
+                ),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight()
+                    .fillMaxSize()
+            )
+        }
+    }
+}
 
+@Composable
+fun LessonList(
+    list: List<String>,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        list.forEach {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(4.dp)
+                    .background(
+                        color = Color(0xFFCEFF95),
+                        shape = RoundedCornerShape(8.dp)
+                    )
+                    .border(
+                        width = 1.dp,
+                        color = Color(0xFF4CAF50),
+                        shape = RoundedCornerShape(8.dp)
+                    )
+                    .padding(top = 8.dp, bottom = 8.dp),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = it,
+                    fontSize = 32.sp,
+                    color = Color(0xFF858585)
+                )
+            }
         }
     }
 }
