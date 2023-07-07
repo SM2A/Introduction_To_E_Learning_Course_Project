bba2b1783a3c7a3e19bc7efd5d9766539782d0a8
Seyed Mohammad Amin Atyabi <seyedamin1990@gmail.com>
2023-07-07 20:01:06 +0330

    Lesson activity navigation buttons

--- app/src/main/java/com/example/tsl/ui/LessonActivity.kt
+++ app/src/main/java/com/example/tsl/ui/LessonActivity.kt
@@ -1,10 +1,19 @@
 package com.example.tsl.ui
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.compose.material.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.res.painterResource
+import com.example.tsl.R
 import com.example.tsl.model.content.Lesson
 import com.example.tsl.util.getDataFromName
 import com.example.tsl.viewmodel.LessonViewModel
@@ -15,10 +24,88 @@ class LessonActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        viewModel.data = getDataFromName(intent.getStringExtra("className")) as Lesson
+
+        viewModel.itemIndex.observe(this) {
+            if (it >= 0) {
+
+            }
+        }
+
         setContent {
-            val data = getDataFromName(intent.getStringExtra("className")) as Lesson
-            Text(text = "Lesson ${data.title}")
+            Column(
+                modifier = Modifier,
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.SpaceEvenly
+            ) {
+                NavigationButtons(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .fillMaxHeight(fraction = 0.2f),
+                    nextAction = {
+                        viewModel.nextItem()
+                    },
+                    prevAction = {
+                        viewModel.previousItem()
+                    }
+                )
+            }
         }
     }
 
+}
+
+@Composable
+fun NavigationButtons(
+    nextAction: () -> Unit,
+    prevAction: () -> Unit,
+    modifier: Modifier = Modifier,
+    showNext: Boolean = true,
+    showPrev: Boolean = true
+) {
+    Row(
+        modifier = modifier,
+        horizontalArrangement = Arrangement.SpaceAround,
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        DirectionButton(
+            modifier = Modifier
+                .fillMaxHeight()
+                .rotate(180f)
+                .weight(1f),
+            isVisible = showPrev,
+            onClickAction = {
+                prevAction.invoke()
+            }
+        )
+        DirectionButton(
+            modifier = Modifier
+                .fillMaxHeight()
+                .weight(1f),
+            isVisible = showNext,
+            onClickAction = {
+                nextAction.invoke()
+            }
+        )
+    }
+}
+
+@Composable
+fun DirectionButton(
+    onClickAction: () -> Unit,
+    modifier: Modifier = Modifier,
+    isVisible: Boolean = true
+) {
+    if (isVisible) {
+        Image(
+            painter = painterResource(
+                id = R.drawable.baseline_navigate_next_24
+            ),
+            contentDescription = "Navigation button",
+            modifier = modifier.clickable {
+                onClickAction.invoke()
+            }
+        )
+    }
 }
--- app/src/main/java/com/example/tsl/viewmodel/LessonViewModel.kt
+++ app/src/main/java/com/example/tsl/viewmodel/LessonViewModel.kt
@@ -1,10 +1,26 @@
 package com.example.tsl.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.tsl.model.content.Lesson
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class LessonViewModel @Inject constructor(): ViewModel() {
+class LessonViewModel @Inject constructor() : ViewModel() {
+
+    lateinit var data: Lesson
+
+    private val itemIndexMutable = MutableLiveData(-1)
+    val itemIndex: LiveData<Int> = itemIndexMutable
+
+    fun nextItem() {
+        itemIndexMutable.value = itemIndexMutable.value?.plus(1)
+    }
+
+    fun previousItem() {
+        itemIndexMutable.value = itemIndexMutable.value?.minus(1)
+    }
 
 }
