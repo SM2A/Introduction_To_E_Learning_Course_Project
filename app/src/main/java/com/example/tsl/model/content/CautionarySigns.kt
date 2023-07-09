7155abe36ae25535c6aa2fa45c3cff86398230a5
atyabi <atyabi@mahsan.co>
2023-07-09 09:22:21 +0330

    Complete cautionary signs lesson

--- app/src/main/java/com/example/tsl/model/content/CautionarySigns.kt
+++ app/src/main/java/com/example/tsl/model/content/CautionarySigns.kt
@@ -8,7 +8,8 @@ object CautionarySigns : Lesson() {
 
     override val title = "تابلوهای هشدار دهنده (اخطاری)"
 
-    override val intro = "در این بخش میخوایم در مورد تایلو های هشداری توضیح بدیم"
+    override val intro =
+        "در این بخش قرار است در مورد تابلو های هشدار دهنده توضیح داده شود\r\nاین تابلو ها به شکل مثلث با حاشیه قرمز رنگ هستند"
 
     override val lessonContent: List<LessonItem> = listOf(
         LessonItem(
@@ -17,11 +18,11 @@ object CautionarySigns : Lesson() {
         ),
         LessonItem(
             image = R.drawable.uneven,
-            description = "تابلو نزدیک شدن به دست انداز"
+            description = "تابلو نزدیک شدن به دست انداز (سرعت گیر)"
         ),
         LessonItem(
             image = R.drawable.narrow_road,
-            description = "تابلو جاده باریک"
+            description = "تابلو جاده باریک می شود"
         ),
         LessonItem(
             image = R.drawable.pedestrian_crossing,
@@ -34,12 +35,33 @@ object CautionarySigns : Lesson() {
         LessonItem(
             image = R.drawable.two_way_street,
             description = "تابلو جاده دو طرفه"
+        ),
+        LessonItem(
+            image = R.drawable.falling_rocks,
+            description = "تابلو هشدار سقوط سنگ"
+        ),
+        LessonItem(
+            image = R.drawable.crossroads,
+            description = "تابلو جاده مارپیچ اولین پیچ به چپ"
+        ),
+        LessonItem(
+            image = R.drawable.bend,
+            description = "تابلو تقاطع مسیر فرعی با اصلی"
+        ),
+        LessonItem(
+            image = R.drawable.slippery_road,
+            description = "تابلو جاده لغزنده"
         )
     )
 
     override val exam = ExamItem(
-        image = R.drawable.danger,
-        question = "این چه تابلوییه ؟",
-        answers = mapOf("خطر" to true, "میدون" to false, "چراغ" to false, "سرعت" to false)
+        image = R.drawable.turn_right,
+        question = "تابلو بالا چه تابلویی است ؟",
+        answers = mapOf(
+            "پیچ به راست" to true,
+            "خطر نزدیک شدن به مار" to false,
+            "راه از چپ باریک می شود" to false,
+            "گردش به راست ممنوع" to false
+        )
     )
 }
